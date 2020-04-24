7d21b784bb63b555f46255d18f633c8c6651f131
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-24 15:33:37 +0200

    Add navigation bar

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.sass']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
