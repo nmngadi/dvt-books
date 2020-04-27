0ace9baaea0032fef9f18877e67188121b53e7e2
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-27 22:48:02 +0200

    Add author list component

---
+++ src/app/author/author-list/author-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { getQueryPredicate } from '@angular/compiler/src/render3/view/util';
+import { IAuthor } from 'src/app/interfaces/Author';
+import { AuthorService } from 'src/app/services/authors.service';
+
+@Component({
+  selector: 'app-author-list',
+  templateUrl: './author-list.component.html',
+  styleUrls: ['./author-list.component.css'],
+})
+export class AuthorListComponent implements OnInit {
+  authors: IAuthor[];
+
+  filteredAuthors: IAuthor[] = [];
+  listfilter = '';
+  get listFilter(): string {
+    return this.listfilter;
+  }
+  set listFilter(value: string) {
+    this.listfilter = value;
+    this.filteredAuthors = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.authors;
+  }
+  constructor(private authorservice: AuthorService) {}
+
+  performFilter(filterBy: string): IAuthor[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.authors.filter(
+      (author: IAuthor) =>
+        author.first_name.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+
+  ngOnInit() {
+    this.authorservice.getAuthors().subscribe({
+      next: (authors) => {
+        this.authors = authors;
+        this.filteredAuthors = this.authors;
+      },
+    });
+  }
+}
