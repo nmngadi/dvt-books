2c48fd4a5ae6947c6131fff4b7f4ae24be190fa0
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-28 15:53:12 +0200

    Remove unnessacary code and inline styling

--- src/app/author/author-create/author-create.component.ts
+++ src/app/author/author-create/author-create.component.ts
@@ -1,55 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 import {
   FormGroup,
   FormBuilder,
   Validators,
   AbstractControl,
-} from '@angular/forms';
-import { AuthorService } from 'src/app/services/authors.service';
-import { IAuthor } from 'src/app/interfaces/author';
+} from "@angular/forms";
+import { AuthorService } from "src/app/services/authors.service";
+import { IAuthor } from "src/app/interfaces/author";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-author-create',
-  templateUrl: './author-create.component.html',
-  styleUrls: ['./author-create.component.css'],
+  selector: "app-author-create",
+  templateUrl: "./author-create.component.html",
+  styleUrls: ["./author-create.component.css"],
 })
 export class AuthorCreateComponent implements OnInit {
   createAuthorForm: FormGroup;
   author: IAuthor;
 
-  constructor(private fb: FormBuilder, private authorservice: AuthorService) {
+  constructor(
+    private fb: FormBuilder,
+    private authorservice: AuthorService,
+    private router: Router
+  ) {
     this.createAuthorForm = this.fb.group({
-      first_name: ['', [Validators.required, Validators.minLength(2)]],
-      middle_names: [''],
-      last_name: ['', Validators.required],
-      name: [''],
-      about: [''],
+      first_name: ["", [Validators.required]],
+      middle_names: [""],
+      last_name: ["", Validators.required],
+      about: [""],
     });
   }
 
   ngOnInit() {}
 
   get first_name(): AbstractControl {
-    return this.createAuthorForm.get('first_name');
+    return this.createAuthorForm.get("first_name");
   }
   get last_name(): AbstractControl {
-    return this.createAuthorForm.get('last_name');
+    return this.createAuthorForm.get("last_name");
   }
   get middle_names(): AbstractControl {
-    return this.createAuthorForm.get('middle_names');
+    return this.createAuthorForm.get("middle_names");
   }
-  get name(): AbstractControl {
-    return this.createAuthorForm.get('name');
-  }
+
   get about(): AbstractControl {
-    return this.createAuthorForm.get('about');
+    return this.createAuthorForm.get("about");
   }
 
   save() {
     this.author = this.createAuthorForm.value;
     this.authorservice.createAuthor(this.author).subscribe();
-    alert(JSON.stringify(this.createAuthorForm.value));
     this.createAuthorForm.reset();
+    this.router.navigate(["/authors"]);
   }
 }
