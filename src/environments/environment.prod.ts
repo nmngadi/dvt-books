7c666a42206047ca2c0fb1028286046ca3b28916
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-28 15:55:18 +0200

    add constants to environments and removed comments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   authorUrl : 'http://localhost:4201/Authors',
   booksUrl: 'http://localhost:4201/Books',
   tagsUrl: 'http://localhost:4201/Tags',
+  domain: 'nmngadi.auth0.com',
+  client_id: 'Sc2Dcm0PRHxbzF9oA5WIcl2zynUn76es',
   production: true
 };
