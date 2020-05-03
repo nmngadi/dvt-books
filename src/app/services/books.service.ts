f5b26b1849e4f2a98e4d7a364fe7f0a5d848f39a
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-03 22:55:03 +0200

    Fix failing test

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IBook } from '../interfaces/books';
 import { environment } from 'src/environments/environment';
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class BooksService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getBooks(query?: string): Observable<IBook[]> {
     return this.http.get<IBook[]>(`${environment.booksUrl}${query ? `?query=${query}` : ''}`);
@@ -17,4 +17,22 @@ export class BooksService {
   createBook(book: IBook): Observable<IBook> {
     return this.http.post<IBook>(environment.booksUrl, book);
   }
+  getBook(isbn13: string): Observable<IBook> {
+    return this.http
+      .get<IBook>(`${environment.booksUrl}/${isbn13}`);
+  }
+  updateBook(book: IBook): Observable<IBook> {
+    return this.http.post<IBook>(environment.booksUrl, book);
+  }
+
+  postPicture(isbn13: string, image: File) {
+    const formData = new FormData();
+    const httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'multipart/form-data'})
+  };
+    formData.append('image', image);
+
+    return this.http.put(`${environment.booksUrl}/${isbn13}/picture`, image, httpOptions);
+  }
+
 }
