a9ddcdbf927452436c708d763219f4d8a9a99998
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-03 02:16:49 +0200

    Add isbn pipe testing

--- src/app/pipes/isbn.pipe.spec.ts
+++ src/app/pipes/isbn.pipe.spec.ts
@@ -1,11 +1,8 @@
-
-
-import { TestBed, async } from '@angular/core/testing';
 import { IsbnPipe } from './isbn.pipe';
-
-describe('Pipe: Isbne', () => {
-  it('create an instance', () => {
-    const pipe = new IsbnPipe();
-    expect(pipe).toBeTruthy();
+describe('IsbnPipe', () => {
+  const pipe = new IsbnPipe();
+  it('should add a prefix', () => {
+    expect(pipe.transform('3864903572', true)).toMatch(/^ISBN-10: /);
+    expect(pipe.transform('9783864903571', true)).toMatch(/^ISBN-13: /);
   });
 });
