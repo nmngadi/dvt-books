88b837e4714d2d2730cf8e2196b775694716e020
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-04 01:18:10 +0200

    Add isbn validation test

---
+++ src/app/book/book-create/book-create.component.spec.ts
@@ -0,0 +1,38 @@
+import { BookCreateComponent } from './book-create.component';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthorService } from 'src/app/services/authors.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('BookCreateComponent', () => {
+  let comp: BookCreateComponent;
+  let fixture: ComponentFixture<BookCreateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BookCreateComponent],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule,
+      ],
+      providers: [{ provide: AuthorService }],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(BookCreateComponent);
+
+        comp = fixture.componentInstance;
+      });
+  }));
+
+  it(`control should be invalid if its not a valid isbn13`, async(() => {
+    comp.createBookForm.controls.isbn13.setValue('nhlelo123');
+    expect(comp.isbn13.valid).toBeFalse();
+  }));
+
+});
