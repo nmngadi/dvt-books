27d9f0537b2f7c6f9d490652e9030b465f4f3557
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-06 10:28:21 +0200

    Remove unused reference

--- src/app/author/author-details/author-details.component.ts
+++ src/app/author/author-details/author-details.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IAuthor } from 'src/app/interfaces/author';
 import { AuthorService } from 'src/app/services/authors.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { booksRef } from 'src/app/interfaces/bookRef';
-import { authorRef } from 'src/app/interfaces/authorRef';
+
 
 @Component({
   selector: 'app-author-details',
