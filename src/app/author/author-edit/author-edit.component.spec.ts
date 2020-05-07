1f0add53b3758a46352207b823b48d6e5907f96a
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-07 17:01:48 +0200

    Add author tests

--- src/app/author/author-edit/author-edit.component.spec.ts
+++ src/app/author/author-edit/author-edit.component.spec.ts
@@ -1,21 +1,21 @@
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
-
-import { BrowserModule, By } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, NgForm, Validators } from '@angular/forms';
 import { AuthorService } from 'src/app/services/authors.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthorEditComponent } from './author-edit.component';
+import { IAuthor } from 'src/app/interfaces/author';
+import { CommonModule } from '@angular/common';
 
 describe('AuthorEditComponent', () => {
   let comp: AuthorEditComponent;
   let fixture: ComponentFixture<AuthorEditComponent>;
-
+  // const spy:any[];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AuthorEditComponent],
       imports: [
-        BrowserModule,
+        CommonModule,
         FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
@@ -28,6 +28,7 @@ describe('AuthorEditComponent', () => {
         fixture = TestBed.createComponent(AuthorEditComponent);
 
         comp = fixture.componentInstance;
+        comp.ngOnInit();
       });
   }));
 
@@ -46,4 +47,16 @@ describe('AuthorEditComponent', () => {
     comp.editAuthorForm.controls.about.setValue('writes about C#');
     expect(comp.editAuthorForm.valid).toBeTruthy();
   }));
+  let authors: IAuthor;
+  authors = {
+    href:
+      'http://localhost:4201/Authors/70088445-6ee2-4745-81d1-8faa4f491658',
+    id: '70088445-6ee2-4745-81d1-8faa4f491658',
+    first_name: 'Jane',
+    middle_names: 'Jill',
+    last_name: 'Doe',
+    name: 'Jane Jill Doe',
+    about: 'writes about romance',
+    books: []
+  };
 });
