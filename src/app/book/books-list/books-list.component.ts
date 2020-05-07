63bf2fba78df1c2fa92cd303196aa94228173751
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-07 17:04:51 +0200

    Add loader on book list component

--- src/app/book/books-list/books-list.component.ts
+++ src/app/book/books-list/books-list.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 import { BooksService } from 'src/app/services/books.service';
-import { startWith, map, switchAll } from 'rxjs/operators';
+import { startWith, map, switchAll, tap } from 'rxjs/operators';
 import { IBook } from 'src/app/interfaces/books';
+import { DomSanitizer } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
-  styleUrls: ['./books-list.component.sass']
+  styleUrls: ['./books-list.component.scss']
 })
 export class BooksListComponent implements OnInit {
   books: IBook[];
   searchForm: FormGroup;
-
-  constructor(private booksservice: BooksService, private fb: FormBuilder) {
+  loading$ = new BehaviorSubject<boolean>(true);
+  constructor(private booksservice: BooksService, private fb: FormBuilder, public auth: AuthService
+  ) {
     this.searchForm = this.fb.group({
       searchStr: [''],
     });
@@ -27,12 +32,15 @@ export class BooksListComponent implements OnInit {
     this.searchstr.valueChanges
       .pipe(
         startWith(''),
+        tap(() => this.loading$.next(true)),
         map((searchstr) => this.booksservice.getBooks(searchstr)),
         switchAll()
       )
       .subscribe((books) => {
         this.books = books;
+        this.loading$.next(false);
       });
-  }
 
+
+  }
 }
