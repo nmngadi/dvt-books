ac6bf304cb3aad8ad4d155599653687c5af60543
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-05-07 16:57:11 +0200

    Add author service changes

--- src/app/services/authors.service.ts
+++ src/app/services/authors.service.ts
@@ -4,19 +4,26 @@ import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { IAuthor } from '../interfaces/author';
-
-
-
 @Injectable({
   providedIn: 'root',
 })
 
 export class AuthorService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+  private searchUrl;
 
-  getAuthors(query?: string): Observable<IAuthor[]> {
+  getAllAuthor(query?: string): Observable<IAuthor[]> {
     return this.http.get<IAuthor[]>(`${environment.authorUrl}${query ? `?query=${query}` : ''}`);
+  }
+
+  getAuthorFilter(skip?: number, top?: number): Observable<IAuthor[]> {
+    this.searchUrl = environment.authorUrl + '?top=' + top + '&skip=' + skip;
+    return this.http.get<IAuthor[]>(this.searchUrl);
+  }
+  getAuthorsSearch(query?: string, skip?: number, top?: number): Observable<IAuthor[]> {
+    this.searchUrl = environment.authorUrl + '?query=' + query + '&top=' + top + '&skip=' + skip;
+    return this.http.get<IAuthor[]>(this.searchUrl);
   }
 
   getAuthor(id: string): Observable<IAuthor> {
