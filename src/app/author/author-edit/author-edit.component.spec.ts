391f0bba6d3d11e9fe8286847376216f5818baf8
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-06-07 23:27:43 +0200

    Add unit tests for author edit

--- src/app/author/author-edit/author-edit.component.spec.ts
+++ src/app/author/author-edit/author-edit.component.spec.ts
@@ -6,11 +6,27 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AuthorEditComponent } from './author-edit.component';
 import { IAuthor } from 'src/app/interfaces/author';
 import { CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
 
 describe('AuthorEditComponent', () => {
   let comp: AuthorEditComponent;
   let fixture: ComponentFixture<AuthorEditComponent>;
-  // const spy:any[];
+  let author: IAuthor;
+  author = {
+    href:
+      'http://localhost:4201/Authors/70088445-6ee2-4745-81d1-8faa4f491658',
+    id: '70088445-6ee2-4745-81d1-8faa4f491658',
+    first_name: 'Jane',
+    middle_names: 'Jill',
+    last_name: 'Doe',
+    name: 'Jane Jill Doe',
+    about: 'writes about romance',
+    books: []
+  };
+  const AuthorServiceMock: any = {
+    updateAuthor(): Observable<IAuthor> { return of(); },
+    getAuthor(): Observable<IAuthor> { return of(author); }
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AuthorEditComponent],
@@ -21,7 +37,7 @@ describe('AuthorEditComponent', () => {
         HttpClientModule,
         RouterTestingModule,
       ],
-      providers: [{ provide: AuthorService }],
+      providers: [{ provide: AuthorService, useValue: AuthorServiceMock }],
     })
       .compileComponents()
       .then(() => {
@@ -47,16 +63,14 @@ describe('AuthorEditComponent', () => {
     comp.editAuthorForm.controls.about.setValue('writes about C#');
     expect(comp.editAuthorForm.valid).toBeTruthy();
   }));
-  let authors: IAuthor;
-  authors = {
-    href:
-      'http://localhost:4201/Authors/70088445-6ee2-4745-81d1-8faa4f491658',
-    id: '70088445-6ee2-4745-81d1-8faa4f491658',
-    first_name: 'Jane',
-    middle_names: 'Jill',
-    last_name: 'Doe',
-    name: 'Jane Jill Doe',
-    about: 'writes about romance',
-    books: []
-  };
+  it(`form should call ngAfterViewInit()`, async(() => {
+    comp.editAuthorForm.controls.firstName.setValue('Jane');
+    comp.editAuthorForm.controls.lastName.setValue('Doe');
+    comp.editAuthorForm.controls.middleNames.setValue('Jill');
+    comp.editAuthorForm.controls.about.setValue('writes about C#');
+    const spy = spyOn(comp, 'ngAfterViewInit').and.callThrough();
+    comp.ngAfterViewInit();
+    expect(spy).toHaveBeenCalled();
+  }));
+
 });
