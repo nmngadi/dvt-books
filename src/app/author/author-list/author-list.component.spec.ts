b5ba229f06504e1b018d6653ab79c936c741af11
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-07-03 17:50:39 +0200

    Add unit tests  on author list

--- src/app/author/author-list/author-list.component.spec.ts
+++ src/app/author/author-list/author-list.component.spec.ts
@@ -9,12 +9,25 @@ import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { IAuthor } from 'src/app/interfaces/author';
 
-describe('AuthorCreateComponent', () => {
+describe('AuthorListComponent', () => {
   let comp: AuthorListComponent;
   let fixture: ComponentFixture<AuthorListComponent>;
   let spy: any;
+  let authors: IAuthor[];
+  authors = [ {
+    href:
+      'http://localhost:4201/Authors/70088445-6ee2-4745-81d1-8faa4f491658',
+    id: '70088445-6ee2-4745-81d1-8faa4f491658',
+    first_name: 'Jane',
+    middle_names: 'Jill',
+    last_name: 'Doe',
+    name: 'Jane Jill Doe',
+    about: 'writes about romance',
+    books: []
+  }];
   const AuthorServiceMock: any = {
-    getAuthorFilter(): Observable<IAuthor[]> { return of(); }
+    getAuthorFilter(): Observable<IAuthor[]> { return of(authors); },
+    getAllAuthor(): Observable<IAuthor[]> { return of(authors); }
   };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,15 +44,54 @@ describe('AuthorCreateComponent', () => {
       .compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(AuthorListComponent);
-
         comp = fixture.componentInstance;
+        comp.searchForm.controls.searchStr.setValue('');
       });
   }));
 
-
+  it('should create', () => {
+    expect(comp).toBeTruthy();
+  });
   it('should call ngOnInit', () => {
     spy = spyOn(comp, 'ngOnInit').and.callThrough();
+
+    fixture.detectChanges();
     comp.ngOnInit();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call search artist', () => {
+    spy = spyOn(AuthorServiceMock, 'getAllAuthor').and.callThrough();
+    comp.ngOnInit();
+    comp.searchAuthor();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call pagininate', () => {
+    spy = spyOn(AuthorServiceMock, 'getAuthorFilter').and.callThrough();
+    comp.ngOnInit();
+    comp.skip = 0;
+    comp.top = 4;
+    AuthorServiceMock.getAuthorFilter();
+    comp.getAuthorsPageinated();
+    expect(spy).toHaveBeenCalled();
+    expect(comp.displayViewMore).toBe(false);
+  });
+  it('should call pagininate else', () => {
+    spy = spyOn(AuthorServiceMock, 'getAuthorFilter').and.callThrough();
+    comp.ngOnInit();
+    comp.skip = 0;
+    comp.top = 0;
+    AuthorServiceMock.getAuthorFilter();
+    comp.getAuthorsPageinated();
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call getMoreAuthors ', () => {
+    spy = spyOn(comp, 'getAuthorsPageinated').and.callThrough();
+    comp.ngOnInit();
+    comp.skip = 0;
+    comp.top = 4;
+    comp.getMoreAuthors();
     expect(spy).toHaveBeenCalled();
   });
 
