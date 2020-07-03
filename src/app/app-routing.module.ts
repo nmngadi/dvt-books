10e5c49cca0a1355e20a1980dea94c54d2411999
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-07-03 17:47:04 +0200

    Add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import { ErrorPageComponent } from './shared/components/error-page/error-page.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'authors',
+    loadChildren: () => import('./author/author.module').then(m => m.AuthorModule)
+  },
+  {
+    path: 'books',
+    loadChildren: () => import('./book/book.module').then(m => m.BookModule)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: ErrorPageComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: true })
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
