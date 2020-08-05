b29c0736cbd4e7932fb65b0d86c3ca15a094f271
NMngadi <NMngadi@DVTL35KH3X2>
2020-08-05 22:37:57 +0200

    Fix linting

--- src/app/pipes/isbn.pipe.spec.ts
+++ src/app/pipes/isbn.pipe.spec.ts
@@ -4,6 +4,5 @@ describe('IsbnPipe', () => {
   it('should add a prefix', () => {
     expect(pipe.transform('3864903572', true)).toMatch(/^ISBN-10: /);
     expect(pipe.transform('9783864903571', true)).toMatch(/^ISBN-13: /);
-    expect(pipe.transform('NaN', true)).toBeNull;
   });
 });
